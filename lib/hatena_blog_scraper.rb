c4064c978c42120bd2dc5197a500426f924eb422
reizist <reizist@gmail.com>
2017-03-10 18:32:24 +0900

    :fire: Remove unnecessary code

--- lib/hatena_blog_scraper.rb
+++ lib/hatena_blog_scraper.rb
@@ -2,6 +2,3 @@ require "hatena_blog_scraper/client"
 require "hatena_blog_scraper/parser"
 require "hatena_blog_scraper/article"
 require "hatena_blog_scraper/version"
-
-module HatenaBlogScraper
-end
