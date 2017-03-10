cc0f80a796b60900340e45c95afcbbc430498fc9
reizist <reizist@gmail.com>
2017-03-10 18:47:20 +0900

    :sparkles: Add function of outputting markdown

--- lib/hatena_blog_scraper/article.rb
+++ lib/hatena_blog_scraper/article.rb
@@ -1,12 +1,14 @@
+require "reverse_markdown"
+
 module HatenaBlogScraper
   class Article
     attr_reader :url, :title, :body, :html, :markdown
-    def initialize(url: nil, title: nil, text: nil, html: nil, markdown: nil)
+    def initialize(url: nil, title: nil, text: nil, html: nil)
       @url      = url
       @title    = title
       @text     = text
       @html     = html
-      @markdown = markdown
+      @markdown = ReverseMarkdown.convert(html)
     end
   end
 end
