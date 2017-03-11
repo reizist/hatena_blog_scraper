aba5f0f7480d37bb23d382f7a4beedd42298f252
reizist <reizist@gmail.com>
2017-03-12 00:39:14 +0900

    :sparkles: Make alias as HBS

--- lib/hatena_blog_scraper.rb
+++ lib/hatena_blog_scraper.rb
@@ -2,3 +2,7 @@ require "hatena_blog_scraper/client"
 require "hatena_blog_scraper/parser"
 require "hatena_blog_scraper/article"
 require "hatena_blog_scraper/version"
+
+
+# make alias
+HBS = HatenaBlogScraper
