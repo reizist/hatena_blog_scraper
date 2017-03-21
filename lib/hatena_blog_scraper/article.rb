c4c03c126447e567fba8be476fea5185ae9ce0f0
reizist <reizist@gmail.com>
2017-03-21 16:11:04 +0900

    :bug: Use text instead of body

--- lib/hatena_blog_scraper/article.rb
+++ lib/hatena_blog_scraper/article.rb
@@ -2,7 +2,7 @@ require "reverse_markdown"
 
 module HatenaBlogScraper
   class Article
-    attr_reader :url, :title, :body, :html, :markdown
+    attr_reader :url, :title, :text, :html, :markdown
     def initialize(url: nil, title: nil, text: nil, html: nil)
       @url      = url
       @title    = title
